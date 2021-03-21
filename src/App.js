16a3c9a4d3f4adc2c62e8dd803054d53e08fe254
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-21 16:48:24 +0100

    Fix indentation

--- src/App.js
+++ src/App.js
@@ -5,51 +5,50 @@ import GifList from './Components/GifList';
 import axios from "axios";
 
 export default class App extends Component {
-  
-  constructor() {
-    super();
-    this.state = {
-        gifs: [],
-        loading: true
+
+    constructor() {
+        super();
+        this.state = {
+            gifs: [],
+            loading: true
+        }
     }
-  }
 
+    componentDidMount() {
+        this.performSearch()
+    }
 
-  componentDidMount() {
-    this.performSearch()
-  }
-
-  performSearch = (query = 'cats') => {
-      axios.get(`https://api.giphy.com/v1/gifs/search?api_key=8kPPAvSPwZGTj0QyLzVO7ovMegXe79v5&q=${query}&limit=24&offset=0&rating=g&lang=en`)
-          .then(response => {
-              this.setState({
-                  gifs: response.data.data,
-                  loading: false
-              })
-          })
-          .catch(error => {
-              console.log('Error fetching and parsing data', error)
-          })
-}
+    performSearch = (query = 'cats') => {
+        axios.get(`https://api.giphy.com/v1/gifs/search?api_key=8kPPAvSPwZGTj0QyLzVO7ovMegXe79v5&q=${query}&limit=24&offset=0&rating=g&lang=en`)
+            .then(response => {
+                this.setState({
+                    gifs: response.data.data,
+                    loading: false
+                })
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error)
+            })
+    }
 
     render() {
-      console.log(this.state.gifs)
-    return (
-      <div>
-        <div className="main-header">
-          <div className="inner">
-            <h1 className="main-title">GifSearch</h1>
-            <SearchForm onSearch={this.performSearch}/>
-          </div>   
-        </div>    
-        <div className="main-content">
-            {
-                (this.state.loading)
-                ? <p>Loading...</p>
-                : <GifList data={this.state.gifs} />
-            }
-        </div>
-      </div>
-    );
-  }
+        console.log(this.state.gifs)
+        return (
+            <div>
+                <div className="main-header">
+                    <div className="inner">
+                        <h1 className="main-title">GifSearch</h1>
+                        <SearchForm onSearch={this.performSearch}/>
+                    </div>
+                </div>
+                <div className="main-content">
+                    {
+                        (this.state.loading)
+                            ? <p>Loading...</p>
+                            : <GifList data={this.state.gifs} />
+                    }
+                </div>
+            </div>
+        );
+    }
 }
