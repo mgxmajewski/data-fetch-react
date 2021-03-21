99eef252b6e6139f5b6b3c372208558d8fc349e7
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-21 16:39:24 +0100

    Add loading paragraph before data is fetched

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-        gifs: []
+        gifs: [],
+        loading: true
     }
   }
 
@@ -21,7 +22,10 @@ export default class App extends Component {
   performSearch = (query = 'cats') => {
       axios.get(`https://api.giphy.com/v1/gifs/search?api_key=8kPPAvSPwZGTj0QyLzVO7ovMegXe79v5&q=${query}&limit=24&offset=0&rating=g&lang=en`)
           .then(response => {
-              this.setState({ gifs: response.data.data })
+              this.setState({
+                  gifs: response.data.data,
+                  loading: false
+              })
           })
           .catch(error => {
               console.log('Error fetching and parsing data', error)
@@ -39,7 +43,11 @@ export default class App extends Component {
           </div>   
         </div>    
         <div className="main-content">
-          <GifList data={this.state.gifs} />
+            {
+                (this.state.loading)
+                ? <p>Loading...</p>
+                : <GifList data={this.state.gifs} />
+            }
         </div>
       </div>
     );
