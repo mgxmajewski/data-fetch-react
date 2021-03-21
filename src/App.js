e73c987213580a590bbdeff316a1b66b88e4279b
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-21 15:29:46 +0100

    Add componentDidMount with fetch to setState

--- src/App.js
+++ src/App.js
@@ -12,6 +12,14 @@ export default class App extends Component {
     }
   } 
 
+  componentDidMount() {
+      fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+          .then(response => response.json())
+          .then(responseData => {
+              this.setState({ gifs: responseData})
+          })
+  }
+
     render() {
     return (
       <div>
